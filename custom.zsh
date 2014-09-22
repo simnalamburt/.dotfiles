23fc806a6154692918e1a2042cb7c9f90246fc50
Hyeon Kim <simnalamburt@gmail.com>
2014-09-22 19:35:29 +0900

    custom.zsh: Added RVM configuration

--- custom.zsh
+++ custom.zsh
@@ -1,15 +1,13 @@
-# Edit $PATH variable
-path() {
-  if [ -d "$1" ]; then
-    export PATH="$1:$PATH"
-  fi
-}
+path() { [[ -d "$1" ]] && export PATH="$1:$PATH" }
+src()  { [[ -s "$1" ]] && source "$1" }
 
-path "/usr/local/bin"
-path "$HOME/.tmux-do/bin"
+
+path "/usr/local/bin"             # OS X
+path "$HOME/.tmux-do/bin"         # tmux-do
+
+# RVM
 path "$HOME/.rvm/bin"
-
-# Set a rvm env variable
+src  "$HOME/.rvm/scripts/rvm"
 export rvmsudo_secure_path=0
 
 # Set a $TERM variable
