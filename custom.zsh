ccb1b315a612033050f46fdf96f45271bac30a43
Hyeon Kim <simnalamburt@gmail.com>
2014-09-25 03:35:12 +0000

    Added 'export' keyword again

--- custom.zsh
+++ custom.zsh
@@ -1,4 +1,4 @@
-path() { [[ -d "$1" ]] && PATH="$1:$PATH" }
+path() { [[ -d "$1" ]] && export PATH="$1:$PATH" }
 src()  { [[ -s "$1" ]] && source "$1" }
 
 
@@ -8,10 +8,10 @@ path "$HOME/.tmux-do/bin"         # tmux-do
 # RVM
 path "$HOME/.rvm/bin"
 src  "$HOME/.rvm/scripts/rvm"
-rvmsudo_secure_path=0
+export rvmsudo_secure_path=0
 
 # Colorful Terminal
-TERM=xterm-256color
+export TERM=xterm-256color
 
 # OCaml
 alias ml='ledit ocaml'
