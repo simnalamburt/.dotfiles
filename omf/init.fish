c9e97ac604df63ba14d67e093314c2dc3599fe11
Hyeon Kim <simnalamburt@gmail.com>
2016-01-29 18:43:34 +0900

    init.fish: hash 종속성 제거

--- omf/init.fish
+++ omf/init.fish
@@ -2,9 +2,9 @@ set -x RUST_BACKTRACE=1
 if not set -q TMUX; set TERM xterm-256color; end
 
 # Aliases
-if hash nvim 2>/dev/null; alias vim='nvim'; end # neovim
-if hash tmux 2>/dev/null; alias irc='tmux attach -t irc'; end
-if hash ag 2>/dev/null; if not hash pt 2>/dev/null; alias pt='ag'; end; end
+if type nvim >/dev/null; alias vim='nvim'; end # neovim
+if type tmux >/dev/null; alias irc='tmux attach -t irc'; end
+if type ag >/dev/null; if not hash pt 2>/dev/null; alias pt='ag'; end; end
 
 # Golang
 set -x GOPATH ~/.go
