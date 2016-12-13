b456e4b9a887bb5eda624899d0a27acab6cc629c
Hyeon Kim <simnalamburt@gmail.com>
2016-12-13 10:48:40 +0900

    $PATH에 경로들 중복으로 들어가는거 방지
    Closes #30

--- config.fish
+++ config.fish
@@ -1,5 +1,24 @@
 if not status --is-interactive; exit; end
 
+function __path
+  switch (count $argv)
+  case 1
+    if perl -e "exit(!(grep(m{^$argv[1]\$},split(':', \$ENV{PATH}))) > 0)"; return; end
+
+    # Append to $PATH
+    set PATH $PATH "$argv[1]" ^/dev/null
+  case 2
+    if perl -e "exit(!(grep(m{^$argv[1]\$},split(':', \$ENV{PATH}))) > 0)"; return; end
+    if [ "$argv[2]" != "--force" ]; return 1; end
+
+    # Prepend to $PATH
+    set PATH "$argv[1]" $PATH ^/dev/null
+  case '*'
+    return 1
+  end
+end
+
+
 #
 # lscolors
 #
@@ -12,7 +31,7 @@ set -x TIME_STYLE "+%y%m%d"
 # Fishrc
 #
 if not set -q TMUX; set TERM xterm-256color; end
-set -x PATH "$HOME/.local/bin" $PATH
+__path "$HOME/.local/bin" --force
 set -x LANG ko_KR.UTF-8
 
 # Aliases
@@ -21,14 +40,14 @@ if type -q tmux; alias irc='tmux attach -t irc'; end
 # Ruby
 if begin; type -q ruby; and type -q gem; end
   set -x GEM_HOME (ruby -e 'print Gem.user_dir')
-  set -x PATH $PATH "$GEM_HOME/bin"
+  __path "$GEM_HOME/bin"
 end
 
 # Golang
 if type -q go
   set -x GOPATH ~/.go
   mkdir -p "$GOPATH/bin"
-  set -x PATH $PATH "$GOPATH/bin"
+  __path "$GOPATH/bin"
 end
 
 # Rust
@@ -36,7 +55,7 @@ if type -q rustc
   set -x RUST_BACKTRACE 1
 end
 if type -q cargo
-  set -x PATH $PATH "$HOME/.cargo/bin" 2> /dev/null
+  __path "$HOME/.cargo/bin"
 end
 
 # chips
@@ -44,7 +63,7 @@ if [ -e ~/.config/chips/build.fish ]; . ~/.config/chips/build.fish ; end
 
 # pyenv
 if [ -d ~/.pyenv ]
-  set -x PATH "$HOME/.pyenv/bin" $PATH
+  __path "$HOME/.pyenv/bin" --force
   . (pyenv init -|psub)
   . (pyenv virtualenv-init -|psub)
 end
@@ -54,3 +73,6 @@ if type -q exa
   alias l="exa -alh --group-directories-first"
   alias ll="exa -lh --group-directories-first"
 end
+
+
+functions --erase __path
