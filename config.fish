c404d6cc7d6e793d315b4076828780126250dc33
Hyeon Kim <simnalamburt@gmail.com>
2017-01-16 15:18:40 +0900

    Update for chips 1.1.1

--- config.fish
+++ config.fish
@@ -59,8 +59,12 @@ if type -q yarn
   __path "$HOME/.config/yarn/global/node_modules/.bin"
 end
 
-# chips
-if [ -e ~/.config/chips/build.fish ]; . ~/.config/chips/build.fish ; end
+# Chips  -  https://github.com/xtendo-org/chips
+if [ -e ~/.config/chips/build.fish ] ; . ~/.config/chips/build.fish ; end
+# Hack for old systems like Ubuntu 14, CentOS 6, etc
+set _ "
+if [ -e ~/.config/chips/build.fish ] ; source ~/.config/chips/build.fish ; end
+"
 
 # pyenv
 if [ -d ~/.pyenv ]
